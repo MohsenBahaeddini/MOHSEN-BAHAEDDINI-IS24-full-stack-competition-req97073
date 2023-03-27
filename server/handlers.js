350fa60223109d0994a5b76c900e8f7ee7c697c2
Mohsen Bahaeddini <mbahaeddinii@gmail.com>
2023-03-27 01:50:20 -0400

    index.js and handlers

---
+++ server/handlers.js
@@ -0,0 +1,155 @@
+// retrieve all products
+const getProducts = (req, res) => {
+  const products = res.locals.products;
+  res.status(200).json(products);
+};
+
+// retrieve a single product by ID
+const getProductById = (req, res) => {
+  const productId = req.params.id;
+  const products = res.locals.products;
+
+  const product = products.find((p) => p.productId === productId);
+
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404).json({
+      error: `Product with ID ${productId} not found`,
+    });
+  }
+};
+
+// add a new product
+const addProduct = (req, res) => {
+  const {
+    productName,
+    productOwnerName,
+    scrumMasterName,
+    developers,
+    startDate,
+    methodology,
+  } = req.body;
+
+  // validate that all required fields are present
+  if (
+    !productName ||
+    !productOwnerName ||
+    !scrumMasterName ||
+    !developers ||
+    !startDate ||
+    !methodology
+  ) {
+    res.status(400).json({
+      error: "All fields are required",
+    });
+    return;
+  }
+
+  const products = res.locals.products;
+
+  // generate a new product ID
+  const maxId = Math.max(...products.map((p) => p.productId), 0);
+  const newProductId = maxId + 1;
+
+  // create the new product object
+  const newProduct = {
+    productId: newProductId,
+    productName,
+    productOwnerName,
+    developers,
+    scrumMasterName,
+    startDate,
+    methodology,
+  };
+
+  // add the new product to the list of products
+  products.push(newProduct);
+
+  // return the new product object
+  res.status(201).json(newProduct);
+};
+
+// edit a specific product
+const editProduct = (req, res) => {
+  const productId = req.params.id;
+  const products = res.locals.products;
+  const product = products.find((p) => p.productId === productId);
+
+  const {
+    productName,
+    scrumMasterName,
+    productOwnerName,
+    developers,
+    methodology,
+  } = req.body;
+
+  if (product) {
+    // Update product details
+    product.productName = productName || product.productName;
+    product.scrumMasterName = scrumMasterName || product.scrumMasterName;
+    product.productOwnerName = productOwnerName || product.productOwnerName;
+    product.developers = developers || product.developers;
+    product.methodology = methodology || product.methodology;
+
+    res.status(200).json({
+      message: `Product with ID ${productId} successfully updated`,
+    });
+  } else {
+    res.status(404).json({
+      error: `Product with ID ${productId} not found`,
+    });
+  }
+};
+
+const findProductsByScrumMasterName = (req, res) => {
+  const products = res.locals.products;
+  const { scrumMasterName } = req.params;
+  console.log(req.params);
+  // Filter the products based on the Scrum Master name
+  const filteredProducts = products.filter((product) =>
+    product.scrumMasterName
+      .toLowerCase()
+      .includes(scrumMasterName.toLowerCase())
+  );
+
+  const totalProducts = filteredProducts.length;
+
+  if (filteredProducts.length) {
+    res.status(200).json({ totalProducts, filteredProducts });
+  } else {
+    res.status(404).json({
+      error:
+        "No product found. Please make sure you have entered the full name of scrum master correctly.",
+    });
+  }
+};
+
+const findProductsByDeveloper = (req, res) => {
+  const products = res.locals.products;
+  const { developerName } = req.params;
+  console.log(req.params);
+  // Filter the products based on the Developer name
+  const filteredProducts = products.filter((product) =>
+    product.developers.includes(developerName)
+  );
+  const totalProducts = filteredProducts.length;
+
+  if (filteredProducts.length) {
+    res.status(200).json({ totalProducts, filteredProducts });
+  } else {
+    res.status(404).json({
+      error:
+        "No product found. Please make sure you have entered the full name of the Developer correctly.",
+    });
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  addProduct,
+  editProduct,
+  findProductsByScrumMasterName,
+  findProductsByDeveloper,
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,81 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+
+const {
+  getProducts,
+  getProductById,
+  addProduct,
+  editProduct,
+  findProductsByScrumMasterName,
+  findProductsByDeveloper,
+} = require("./handlers");
+
 const port = process.env.PORT || 5000;
 
+// placing the users in memory | a poor man's database ;)
+// any changes to the this data will persist only until the server restarts.
+const products = require("./data/products.json");
+
+// this function add the products array to the res object so that subsequent
+// functions can access it via res.locals.products.
+// We need to do this because the handlers are in a different file
+// and don't have access to the products variable that was declared in index.js.
+const passProductsAlong = (req, res, next) => {
+  res.locals.products = products; // adds products to the res
+  next(); // this passes the req, and res to the next handler in chain
+};
+
+const app = express();
+
+app.use(morgan("tiny"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+// global middleware to add products to response locals
+app.use(passProductsAlong);
+
+// REST endpoints
+//=======================================
+
+// health endpoint
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "API Component is healthy ✔",
+  });
 });
 
+// retrieve all products
+app.get("/api/products", getProducts);
+
+// retrieve a single product by ID
+app.get("/api/products/:id", getProductById);
+
+// retrieve products that CRRENTLY ARE BEING DEVELOPED OR MAINTAINED
+
+// add a new product
+app.post("/api/new-product", addProduct);
+
+// edit a specific product
+app.patch("/api/products/:id", editProduct);
+
+// retrieve products by Scrum Master name
+app.get(
+  "/api/products/scrum-masters/:scrumMasterName",
+  findProductsByScrumMasterName
+);
+
+// retrieve products by Developer name
+app.get("/api/products/developers/:developerName", findProductsByDeveloper);
+
+// this is our catch all endpoint.
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "Not Found",
+  });
+});
+
+// Node spins up the server and sets it to listen on port 5000 or process.env.PORT
 app.listen(port, () => {
   console.log(`Server is listening at port: ${port}`);
 });
